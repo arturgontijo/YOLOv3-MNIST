f0f780de693dd8481e0a2274c962bad34c4357e4
arturgontijo <arturgontijo@gmail.com>
2018-10-26 09:07:57 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # YOLOv3-MNIST
+
+Train and Test YOLOv3 to detect handwritten digits using MNIST Database.
+
+- Train:
+```
+./darknet/darknet detector train cfg/mnist.dataset cfg/yolov3_mnist.cfg darknet53.conv.74
+```
+
+- Test:
+```
+./darknet/darknet detect cfg/yolov3_mnist.cfg models/yolov3_mnist.backup IMAGE_PATH
+```
---
+++ utils/mnist_labels.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageOps
+import glob
+import re
+
+
+def labels_64_64(size=(64, 64)):
+    img_files = glob.glob("./data/mnist/images/*.png")
+    tam = len(img_files)
+    for idx, file_path in enumerate(img_files):
+        x = 0.5
+        y = 0.5
+        w = 28 / size[0]
+        h = 28 / size[1]
+        m = re.search('(\_c)([0-9])', file_path)
+        c = m.group(2)
+        # <object-class> <x> <y> <width> <height>
+        #        c        x   y     w       h
+        txt_name = file_path.split("/")[-1]
+        txt_name = txt_name.split(".")[0] + ".txt"
+        txt_path = "./data/mnist/labels/{}".format(txt_name)
+        with open(txt_path, "w") as f:
+            data = "{} {} {} {} {}".format(c, x, y, w, h)
+            f.write(data)
+            print("[{}/{}] {} done!".format(idx, tam, txt_path))
+
+
+def resize(new_size=(64, 64), invert=False):
+    img_files = glob.glob("./data/mnist/images/*.png")
+    tam = len(img_files)
+    for idx, file_path in enumerate(img_files):
+        old_im = Image.open(file_path)
+        w, h = old_im.size
+        im = Image.new("RGB", new_size)
+        im.paste(old_im, (int((new_size[0]-w)/2), int((new_size[1]-h)/2)))
+        if invert:
+            im = ImageOps.invert(im)
+        im.save(file_path, "JPEG")
+        print("[{}/{}] {} done!".format(idx, tam, file_path))
