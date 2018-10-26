700b4aaf9657acee88dc245e35a3f6550d0f9617
arturgontijo <arturgontijo@gmail.com>
2018-10-26 10:40:51 -0300

    requirements.txt

--- utils/download_and_convert_mnist.py
+++ utils/download_and_convert_mnist.py
@@ -6,7 +6,7 @@ import cv2
 import os
 
 
-def conv_mnist(img_file,label_file, num, path, list,label):
+def conv_mnist(img_file, label_file, num, path, list_file, label):
 
     imgs = open(img_file, "rb").read()
     imgs = np.fromstring(imgs, dtype=np.uint8)
@@ -15,9 +15,9 @@ def conv_mnist(img_file,label_file, num, path, list,label):
 
     labels = open(label_file,"rb").read()
     labels = np.fromstring(labels, dtype=np.uint8)
-    labels = labels[8:] # skip header
+    labels = labels[8:]
 
-    fw = open(list, "w")
+    fw = open(list_file, "w")
     for i in range(num):
         class_id = labels[i]
         img_name = "%s_%05d_c%d.png" % (label, i, class_id)
@@ -27,17 +27,7 @@ def conv_mnist(img_file,label_file, num, path, list,label):
     fw.close()
 
 
-data_files = ["train-images-idx3-ubyte.gz",
-              "train-labels-idx1-ubyte.gz",
-              "t10k-images-idx3-ubyte.gz",
-              "t10k-labels-idx1-ubyte.gz"]
-
 cwd = os.getcwd()
-for data_file in data_files:
-    data_path = cwd + "/data/" + data_file
-    if not os.path.exists(data_path):
-        os.system('curl -O {} http://yann.lecun.com/exdb/mnist/'.format(data_path, data_file))
-        os.system('gunzip ' + data_file)
 
 mnist_folder = cwd + "/data/mnist"
 img_path = cwd + "/data/mnist/images"
@@ -47,5 +37,25 @@ if not os.path.exists(img_path):
         os.mkdir(mnist_folder)
     os.mkdir(img_path)
 
-conv_mnist("t10k-images-idx3-ubyte", "t10k-labels-idx1-ubyte", 10000, img_path, "mnist.valid.list","v")
-conv_mnist("train-images-idx3-ubyte", "train-labels-idx1-ubyte", 60000, img_path, "mnist.train.list","t")
+t = [cwd + "/data/" + "t10k-images-idx3-ubyte",
+     cwd + "/data/" + "t10k-labels-idx1-ubyte",
+     cwd + "/data/" + "train-images-idx3-ubyte",
+     cwd + "/data/" + "train-labels-idx1-ubyte"]
+
+data_files = ["train-images-idx3-ubyte.gz",
+              "train-labels-idx1-ubyte.gz",
+              "t10k-images-idx3-ubyte.gz",
+              "t10k-labels-idx1-ubyte.gz"]
+
+for data_file in data_files:
+    data_path = cwd + "/data/" + data_file
+    if not os.path.exists(data_path):
+        os.system('curl -O http://yann.lecun.com/exdb/mnist/{}'.format(data_file))
+        os.system('gunzip ' + data_file)
+
+for file in t:
+    curr_file = file.split("/")[-1]
+    os.rename(curr_file, file)
+
+conv_mnist(t[0], t[1], 10000, img_path, "data/mnist/mnist.valid.list", "v")
+conv_mnist(t[2], t[3], 60000, img_path, "data/mnist/mnist.train.list", "t")
